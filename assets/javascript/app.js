9ebaf6903f4f4badba1428378352c8d8b1c89252
Nathan Hill <pjsstp@gmail.com>
2019-06-28 13:21:55 -0400

    commit with initial files, styles, and html done

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+  // Your web app's Firebase configuration
+var firebaseConfig = {
+authDomain: "traintimes-2a979.firebaseapp.com",
+databaseURL: "https://traintimes-2a979.firebaseio.com",
+projectId: "traintimes-2a979",
+storageBucket: "",
+messagingSenderId: "20008059544",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
