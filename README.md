c46ce5a6273e7a87f3b9b0d5f51fc218476c2e4e
Nathan Hill <pjsstp@gmail.com>
2019-07-03 09:46:07 -0400

    added comments into all files, completed readME file

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Train_Times
-
-
-
-
-
-
-
+# Platform 9 & 7/16
 
 
 Background Photo by <a style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px" href="https://unsplash.com/@icreatelife_?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Kristina Kashtanova"><span style="display:inline-block;padding:2px 3px"><svg xmlns="http://www.w3.org/2000/svg" style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white" viewBox="0 0 32 32"><title>unsplash-logo</title><path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path></svg></span><span style="display:inline-block;padding:2px 3px">Kristina Kashtanova</span></a>
+
+<img src="assets/images/deathly_hallows.png" style="float:left; margin:10px;">
+<p style="font-size: 16pt">Platform Nine and Seven Sixteenths was created to complete a homework assignment for the Penn LPS coding bootcamp. The project pushes and deletes from a firebase database using information provided in the form on the website. Users may add their own train by entering a train name, destination, time and frequency (in minutes) and the information will be added to the database and updated immediately on the website.</p> 
+
+<p style="font-size: 16pt">The website is set to update once every second, essentially showing the information in real time.  Users may also delete a train from the database by clicking on the 'Remove' button along the right hand side of the page. Alerts are displayed using a modal div that is automatically centered on the page each time it is displayed based on the height and width of the window.</p>
+
+<p style="font-size: 16pt">Thank you for visiting Platform 9 and Seven Sixteenths!</p>
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -45,23 +45,36 @@ $("#add-train").on("click", function(event) {
 });
 
 $(document).on('click', '.remove-train', function() {
+    // Dom listener to listen for any "Remove" button to be clicked. 
+    // When it detects a click, it pulls the key info from the attribute "data-key" 
+    //   and then calls into the firebase database to delete that key and all of it's 
+    //   child attributes. 
     var keyToRemove = $(this).attr('data-key');
     database.ref(keyToRemove).remove();
 })
 
 $("#clear-alert").on('click', function() {
+    // Click listener for the "OK" button in the alert box to be clicked. 
+    // When it is cleared, this hides the alert until it is needed again.
     event.preventDefault();
     $(".my-alert").hide();
 });
 
+// Timer to refresh the list of trains once every second. 
 var refreshTimer = setInterval(dataSnapshot, 1000 );
 
-
-
 }); 
-//// end of document onload function for DOM manipulation
+//------------------------------------------------------------|
+//// end of document onload function for DOM manipulation     |
+//------------------------------------------------------------|
 
 database.ref().on("child_added", function(snapshot) {
+    // Listener function for a new child node to be added to the firebase database. 
+    // When a child node is added, this function will redraw the train table with 
+    //   all of the prior data nodes as well as the new node. 
+
+    // Here are the initial variables to draw the table row and calculate the cells
+    //   that need to be calculated. 
     var newRow = $("<tr>");
     var tempCell = $("<td>");
     var freq = parseInt(snapshot.val().frequency);
@@ -89,18 +102,24 @@ database.ref().on("child_added", function(snapshot) {
     newRow.append(tempCell);
     tempCell = $("<td>");   // cell must be reset for the next cell to be built
          
+    // setting initial variables to do time calculations
     var tempTime = makeInt(snapshot.val().startTime);
     var timeToArrival = 0;
 
+    // Calculating the next arrival time, without using moment (except for setting the current time)
+    //   because I could not - for the life of me - get moment.js to do what I wanted it to do. I ended
+    //   up changing all of the times to integers and doing the calculations with the times as integers 
+    //   and then figuring out how that time parses out into minutes and hours after all of the 
+    //   calculations are done. 
     if (tempTime < currentTime) {
-        // for start time before current time
+        // for start time before current time, need to work backwards from now until we get to the next train.
         while (tempTime < currentTime) {
             tempTime = tempTime + freq;
         }
         timeToArrival =  tempTime - currentTime
     }
-    // for start time after current time
     else {
+        // for start time after current time, need to work forward from now until we get to the next train. 
         timeToArrival =  tempTime - currentTime
         while (timeToArrival > freq){
             tempTime -= freq;
@@ -113,58 +132,72 @@ database.ref().on("child_added", function(snapshot) {
     newRow.append(tempCell);
     tempCell = $("<td>");   // cell must be reset for the next cell to be built
 
+    // If the minutes to arrival is equal to zero, display a special message. 
     if (timeToArrival === 0) {
         tempCell.html("<span class='now'>Arriving Now</span>");
     }
+    // otherwise, show the minutes to arrival. 
     else {tempCell.text(timeToArrival);}
     
     newRow.append(tempCell);
     tempCell = $("<td>");
 
-
+    // Make the "Remove" button for the last cell in the table. 
     var newButton = $("<button>");
+    // pull in the node key and put into the attribute 'data-key' so that the button functions properly
+    //   when it is clicked. 
     newButton.attr('data-key', snapshot.key);
+    // give appropriate classes to display and allow dom click listener to recognize when the button is clicked.
     newButton.addClass('btn btn-secondary p-1 remove-train');
     newButton.text('Remove').$
 
+    // append the button to the row
     tempCell.append(newButton);
     newRow.append(tempCell);
 
-    // append the data into our table
+    // append the new row into our table
     $("#train-table").append(newRow);
 });
 
 function parseTime(int) {
+    // This is a self-written function to parse the time into a readable format from an integer that represents 
+    //   the total minutes that have elapsed since midnight. This data is already validated, so the format will 
+    //   either be in "H:mm" or "HH:mm" format, and this function will return in the same format. 
     var hrs = Math.floor(int / 60);
     var min = int % 60;
     if (hrs > 23) {
+        // if we have gone over to the next day, remove 24 hours from the time
         hrs -= 24;
     }
-
     if (hrs < 10){
         hrsTxt = "0" + hrs
     }
     else {
         hrsTxt = hrs;
     }
-
     if (min < 10) {
         var minStr = "0" + min;
     }
     else  {
         var minStr = min;
     }   
+    // put the time together into a string
     var returnString = hrsTxt + ":" + minStr;
+
+    //return the string to complete the function. 
     return returnString; 
 }
 
 function makeInt(timeString) {
+    // take a time and change it into an integer 
     var hrs;
     var min;
     if (timeString.length === 4) {
+        // if the time is in "H:mm" format
         hrs = parseInt(timeString.substr(0,1))
         min = parseInt(timeString.substr(2,2))
     } else {
+        // if the time is in "HH:mm" format
         hrs = parseInt(timeString.substr(0,2));
         min = parseInt(timeString.substr(3,2));
     }
@@ -184,6 +217,7 @@ function isValidTime(testString){
         validTime = false;
     }
     else if(testString.length === 4) {
+        // if the string is a correct legth, determine if it is an integer, colon, and integer
         if (typeof(parseInt(testString.substr(0,1))) === 'number' && 
             testString.substr(1,1) === ":" && 
             typeof(parseInt(testString.substr(2,2))) === 'number' ) {
@@ -194,6 +228,7 @@ function isValidTime(testString){
         }
     }
     else if(testString.length === 5) {
+        // if the string is a correct legth, determine if it is an integer, colon, and integer
         if (typeof(parseInt(testString.substr(0,2))) === 'number' && 
             testString.substr(2,1) === ":" && 
             typeof(parseInt(testString.substr(3,2))) === 'number' ) {
@@ -204,26 +239,37 @@ function isValidTime(testString){
         } 
     }
     else {
+        // fail for everything else
         validTime = false;
     }
     return validTime;
 }
 
 function showAlert(alertText) {
+    // This function shows a modal alert div, and places the alertText into the display div. 
+
+    // define the variables that center the div on the page based on the height and width of the page
     var alertTop = Math.floor((($(window).height())/2)-50);
     var alertLeft = Math.floor((($(window).width())/2) - 175);
-    alert("alertTop: " + alertTop);
-    alert("alertLeft: " + alertLeft);
+
+    // set the alert text
     $(".alert-text").text(alertText);
+
+    // set the left and top attributes of the div to re-center the div in the wondow
     $(".my-alert").css('top', alertTop);
     $(".my-alert").css('left', alertLeft);
+
+    //show the div as a modal alert
     $(".my-alert").show();
 }
 
 function dataSnapshot() {
+    // do a one-time call onto the database and take a snapshot of the whole thing
     database.ref().once('value').then(function(snapshot) {
+        // empty the train div to allow for re-writing all of the table rows
         $("#train-table").empty();
         snapshot.forEach(function(childSnap) {
+            // loop through each key value in the database, and write each row. 
             buildRow(childSnap)
         })
     })
@@ -251,25 +297,33 @@ function buildRow(snapshot) {
     //build and append the start time cell
     tempCell.text(snapshot.val().startTime);
     newRow.append(tempCell);
+    tempCell.addClass('text-center');
     tempCell = $("<td>");   // cell must be reset for the next cell to be built
 
     // build and append the frequency cell
     tempCell.text(freq);
     newRow.append(tempCell);
+    tempCell.addClass('text-center');
     tempCell = $("<td>");   // cell must be reset for the next cell to be built
          
+    // setting initial variables to do time calculations
     var tempTime = makeInt(snapshot.val().startTime);
     var timeToArrival = 0;
 
+    // Calculating the next arrival time, without using moment (except for setting the current time)
+    //   because I could not - for the life of me - get moment.js to do what I wanted it to do. I ended
+    //   up changing all of the times to integers and doing the calculations with the times as integers 
+    //   and then figuring out how that time parses out into minutes and hours after all of the 
+    //   calculations are done. 
     if (tempTime < currentTime) {
-        // for start time before current time
+        // for start time before current time, need to work backwards from now until we get to the next train.
         while (tempTime < currentTime) {
             tempTime = tempTime + freq;
         }
         timeToArrival =  tempTime - currentTime
     }
-    // for start time after current time
     else {
+        // for start time after current time, need to work forward from now until we get to the next train. 
         timeToArrival =  tempTime - currentTime
         while (timeToArrival > freq){
             tempTime -= freq;
@@ -279,26 +333,35 @@ function buildRow(snapshot) {
 
     // build and append the next arrival cell
     tempCell.text(parseTime(tempTime));
+    tempCell.addClass('text-center');
     newRow.append(tempCell);
     tempCell = $("<td>");   // cell must be reset for the next cell to be built
 
+    // If the minutes to arrival is equal to zero, display a special message. 
     if (timeToArrival === 0) {
         tempCell.html("<span class='now'>Arriving Now</span>");
     }
+    // otherwise, show the minutes to arrival. 
     else {tempCell.text(timeToArrival);}
-    
+
     newRow.append(tempCell);
+    tempCell.addClass('text-center');
     tempCell = $("<td>");
 
-
+    // Make the "Remove" button for the last cell in the table. 
     var newButton = $("<button>");
+    // pull in the node key and put into the attribute 'data-key' so that the button functions properly
+    //   when it is clicked. 
     newButton.attr('data-key', snapshot.key);
+    // give appropriate classes to display and allow dom click listener to recognize when the button is clicked.
     newButton.addClass('btn btn-secondary p-1 remove-train');
     newButton.text('Remove').$
 
+    // append the button to the row
     tempCell.append(newButton);
+    tempCell.addClass('text-center');
     newRow.append(tempCell);
 
-    // append the data into our table
+    // append the new row into our table
     $("#train-table").append(newRow);
 }
